cb57c7aabb7cf6b3bc744235cd5b4aefc8245e77
mayurjungari <mayurjungari1@gmail.com>
2023-10-05 12:16:54 +0530

    one to may relation added

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'Node_complete',
-    password: 'Mayur@123'
+const sequelize = new Sequelize('node_complete', 'root', 'Mayur@123', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
